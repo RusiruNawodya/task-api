6a584f395367dc74b6fd395fcf5f478a7ba970c2
Rusiru Nawodya <rusirunawo2001@gmail.com>
2025-11-03 01:49:52 +0530

    Update server.js
    add route path and errorHandler path

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const taskRoutes = require('./Routes/TaskRoutes');
+const { globalErrorHandler } = require('./Middleware/errorHandler');
 
 const app = express();
 
 app.use(express.json());
+
+// Routes
+app.use('/tasks', taskRoutes);
+
+// Error handler
+app.use(globalErrorHandler);
 
 mongoose.connect(process.env.MONGO_URI)
 
